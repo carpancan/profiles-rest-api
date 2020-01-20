71e20be615d583d0dee7863d419bb267676eb42f
Carlos Paniagua Cano <carlos.paniagua@sngular.team>
2020-01-20 17:46:25 +0100

    Added HellowApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HellowApiView(APIView):
+    """APIView example"""
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'Uno',
+            'Dos',
+            'Tres',
+            'Cuatro',
+        ]
+
+        return Response({
+            'message':  'Hellow', 'an_apiview': an_apiview
+        }, 200)
