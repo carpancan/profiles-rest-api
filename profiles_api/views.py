580a1708c7d49c0474ff14931a18ccd77345f566
Carlos Paniagua Cano <carlos.paniagua@sngular.team>
2020-01-22 11:00:12 +0100

    Added login by Token in API Rest

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from rest_framework import filters
 from profiles_api import serializers
 from profiles_api import models
@@ -93,3 +95,8 @@ class UserProfileViewSet(ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile, )
     filter_backends = (filters.SearchFilter, )
     search_fields = ('name', 'email', )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
